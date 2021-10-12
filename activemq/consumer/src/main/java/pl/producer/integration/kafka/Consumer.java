c1d5165c0e78bdb19a4106247b834b8de30bc359
niedzii <niedzwiad14@gmail.com>
2021-10-12 19:34:19 +0200

    removed unused dependencies

---
+++ activemq/consumer/src/main/java/pl/producer/integration/kafka/Consumer.java
@@ -0,0 +1,34 @@
+package pl.producer.integration.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import pl.producer.service.LocalTimeService;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class Consumer {
+
+  private static final String TOPIC_NAME = "test";
+
+  private final LocalTimeService timeService;
+
+  @KafkaListener(topics = TOPIC_NAME)
+  public void listen(String message) {
+    log.info(
+        String.format(
+            "Received message on topic %s: %s, delay is %s milliseconds",
+            TOPIC_NAME, message, calculateDelay(message)));
+  }
+
+  private Long calculateDelay(String message) {
+    LocalTime messageTime = LocalTime.parse(message);
+    LocalTime now = timeService.now();
+    return Duration.between(messageTime, now).toMillis();
+  }
+}
